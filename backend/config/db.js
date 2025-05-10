b63153f5888d6b662fdf03fa38ed20cb9a33fbcc
giovannad13 <110957157+giovannad13@users.noreply.github.com>
2025-05-10 11:35:33 -0700

    Fix: ignore node_modules

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectDB() {
-    await mongoose.connect('mongodb+srv://giovanna89:G@dev1@cluster0.tazti.mongodb.net/food-delivery').then(console.log("DB Connected"));
-}
